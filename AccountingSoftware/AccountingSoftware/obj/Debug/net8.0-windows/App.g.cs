1e82cdab546a4cb90e90e616b4a8e0fa0a693d5a
yongqin123 <yongqin12324@gmail.com>
2025-06-27 21:24:07 -0700

    update

--- AccountingSoftware/AccountingSoftware/obj/Debug/net8.0-windows/App.g.cs
+++ AccountingSoftware/AccountingSoftware/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace AccountingSoftware {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace AccountingSoftware {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
         public static void Main() {
             AccountingSoftware.App app = new AccountingSoftware.App();
             app.InitializeComponent();
